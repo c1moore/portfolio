368d6e257e4f2e92eb12f7ae098a75601dc1aff3
Calvin Moore <c1moore@outlook.com>
2018-04-13 14:39:02 -0400

    Added new Components to ComponentsModule.

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,27 +1,38 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { LoadingEllipsisComponent } from './loading-ellipsis/loading-ellipsis.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroImgComponent } from './hero-img/hero-img.component';
 import { TextTyperComponent } from './text-typer/text-typer.component';
+import { ProgrammingLanguageComponent } from './programming-language/programming-language.component';
+import { SkillComponent } from './skill/skill.component';
+import { CreditComponent } from './credit/credit.component';
 
 
 @NgModule({
 	imports:			[
-		CommonModule
+		CommonModule,
+		RouterModule
 	],
 	declarations:	[
 		LoadingEllipsisComponent,
 		NavbarComponent,
 		HeroImgComponent,
-		TextTyperComponent
+		TextTyperComponent,
+		ProgrammingLanguageComponent,
+		SkillComponent,
+		CreditComponent
 	],
 	exports:			[
 		LoadingEllipsisComponent,
 		NavbarComponent,
 		HeroImgComponent,
-		TextTyperComponent
+		TextTyperComponent,
+		ProgrammingLanguageComponent,
+		SkillComponent,
+		CreditComponent
 	]
 })
 export class ComponentsModule { }
